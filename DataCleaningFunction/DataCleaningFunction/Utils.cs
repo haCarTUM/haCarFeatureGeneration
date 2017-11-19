e5e308e15e0e512c888eab514dad5177f8f6afa7
Florian Baader <flobaader@web.de>
2017-11-19 09:26:45 +0100

    Add featur function

---
+++ DataCleaningFunction/DataCleaningFunction/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCleaningFunction
+{
+    static class Utils
+    {
+        public static float toAbs(this float f)
+        {
+            return Math.Abs(f);
+        }
+
+        public static float qu(this float f)
+        {
+            return (float) Math.Pow(f, 2);
+        }
+    }
+}
